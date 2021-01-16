fa4d6fc2d69f2399d5cac77dcb432b1128f1d163
SarthakAdhikari <adhikari.sarthak@gmail.com>
2021-01-16 13:34:09 +0545

    Add seperation between site name and RSS url

--- feed/models.py
+++ feed/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from core.models import User
+import tldextract
 
 class Site(models.Model):
-   url = models.URLField()
+   rss_url = models.URLField()
+   site_url = models.CharField(primary_key=True, max_length=200, unique=True, blank=True)
+
+   def save(self, *args, **kwargs):
+      if getattr(self, 'rss_url'):
+         self.site_url = ".".join(part for part in tldextract.extract(self.rss_url) if part)
+         super().save(*args,**kwargs)
 
    def __str__(self):
-       return self.url
+       return self.site_url
 
 class Topic(models.Model):
     name = models.CharField(max_length=100)
@@ -19,15 +26,14 @@ class Subscription(models.Model):
     topic = models.ManyToManyField(Topic)
 
 
-
 class Post(models.Model):
    title = models.CharField(max_length = 100)
    summary = models.TextField()
    link = models.URLField()
    published = models.DateTimeField()
-
+   site = models.ForeignKey(Site, on_delete=models.CASCADE)
 
    def __str__(self):
        return f'{self.post_id} {self.title}'
 
-import feed.fetch
+# import feed.fetch

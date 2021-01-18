324adecc0081ac3562d0b547b7e3b2ccd301d901
SarthakAdhikari <adhikari.sarthak@gmail.com>
2021-01-18 09:19:57 +0545

    Add subscription API

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from rest_framework import routers
 
 from api.v1.views.core import CreateUserView, LoginView
-from api.v1.views.feed import FeedView
+from api.v1.views.feed import FeedView, SubscriptionView
 
 urlpatterns = [
     path('login/', LoginView.as_view() , name="login"),
     path('signup/', CreateUserView.as_view(), name="signup"),
-    path('feed/', FeedView.as_view(), name="feed")
+    path('feed/', FeedView.as_view(), name="feed"),
+    path('subscription/', SubscriptionView.as_view(), name="subscribe"),
 ]
--- api/v1/views/feed.py
+++ api/v1/views/feed.py
@@ -1,7 +1,14 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 
+from feed.models import Topic, Subscription
+from feed.serializers import SubscriptionSerializer
+
+class SubscriptionView(generics.CreateAPIView, generics.DestroyAPIView):
+    serializer_class = SubscriptionSerializer
+    queryset = Subscription.objects.all()
+
+
 class FeedView(APIView):
     def get(self, request, *args, **kwargs):
-       user = request.user
-       pass
+        topics = [topic.name for topic in Subscription.objects.get(user=request.user).topic.all()]
